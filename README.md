339defbb2d7ee2096e49c3e874bc3ca299c7c99b
Abhiroop Talasila <abhiroop.talasila@gmail.com>
2020-05-23 12:49:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# HashChecker
+
+- [HashChecker](#hashchecker)
+  - [About](#about)
+  - [References](#references)
+
+---
+
+## About
+This project is aimed to provide a unified application to check integrity of files/folders after they're copied/moved
+
+
+## References
+1. https://medium.com/financeexplained/3-quick-ways-to-compare-data-in-python-65201be10b6
+2. https://towardsdatascience.com/learn-how-to-quickly-create-uis-in-python-a97ae1394d5
+
+
---
+++ intcheck.py
@@ -0,0 +1,54 @@
+"""
+Created By: Abhiroop Talasila
+Date: May 23 2020
+Description: This module checks the integrity of data
+
+"""
+
+import hashlib
+import sys
+import os
+
+
+def check_md5(fname):
+    md5hash = hashlib.md5()
+    with open(fname) as handle:  # Consider one line in the file at a time 
+        for line in handle:
+            md5hash.update(line.encode('utf-8'))
+    return md5hash.hexdigest()
+
+def check_sha1(fname):
+    sha1hash = hashlib.sha1()
+    with open(fname) as handle:
+        for line in handle:
+            sha1hash.update(line.encode('utf-8'))
+    return sha1hash.hexdigest()
+
+def check_sha512(fname):
+    sha512hash = hashlib.sha512()
+    with open(fname) as handle:
+        for line in handle:
+            sha512hash.update(line.encode('utf-8'))
+    return sha512hash.hexdigest()
+
+
+if __name__ == "__main__":
+    files = [sys.argv[1], sys.argv[2]] # input arguments 
+
+    print("Comparing Files:", files[0], 'and', files[1])
+
+    if check_md5(files[0]) == check_md5(files[1]):
+        print('MD5 Matched')
+    else: 
+        print('MD5 Not Matched')
+
+    if check_sha1(files[0]) == check_sha1(files[1]):
+        print('SHA1 Matched')
+    else: 
+        print('SHA1 Not Matched')
+
+    if check_sha512(files[0]) == check_sha512(files[1]):
+        print('SHA512 Matched')
+    else: 
+        print('SHA512 Not Matched')
+    
